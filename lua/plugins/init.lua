5a866b9502d44f12e77e1ba539d59bf216fbca7c
Joel Bondurant <em@joelbondurant.com>
2025-11-17 04:03:53 -0600

    Rust debugger adapter added.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,8 +13,46 @@ return {
 
   {
     'mrcjkb/rustaceanvim',
-    version = "^5",
+    version = "^6",
     lazy = false,
-  }
+    config = function ()
+      local extension_path = vim.fn.expand("~/.local/share/nvim/mason/packages/codelldb/extension")
+      local codelldb_path = extension_path .. "/adapter/codelldb"
+      local liblldb_path = extension_path .. "/lldb/lib/liblldb.so"
+      local cfg = require('rustaceanvim.config')
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function ()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function ()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require("dapui").setup()
+    end,
+  },
 
 }
